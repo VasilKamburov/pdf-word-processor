e19c614ff1768cd332e3e55bc780d6273e3c39ad
VasilKamburov <tulevskiv@gmail.com>
2024-06-11 17:50:03 +0300

    - initial commit
    (cherry picked from commit 7748f693ee4901638cce90a910a2e1818d80c47b)

---
+++ src/main/java/com/vtk/pdf_word_processor/DictionaryCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.vtk.pdf_word_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryCreatorApplication.class, args);
+	}
+
+}
